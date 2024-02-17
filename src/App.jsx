def98955379b80a5cda1aa26cff5dc01ada2a09e
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-02-17 17:19:22 +0530

    code complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,30 @@ import "./App.css";
 import { useGetPostsQuery, useNewPostMutation } from "./redux/api";
 import PostCard from "./components/PostCard";
 import { useState } from "react";
+import NewPostCard from "./components/NewPostCard";
 
 function App() {
-  const { isLoading, data } = useGetPostsQuery();
+  const {  data } = useGetPostsQuery();
   let products = data?.products;
-  
-
   const [newPost]=useNewPostMutation()
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
 
+  const [newProducts,setNewProducts]=useState([])
   function handleClick(e) {
     e.preventDefault();
-    console.log(name, description, price);
-   
-
     const product={
       name,description,price
     }
+    setNewProducts(prev=>prev.concat(product))
+
     newPost(product)
      setName("");
      setDescription("");
      setPrice("");
   }
-  return (
+  return (<>
     <div className="flex flex-col justify-center gap-4 items-center mt-4">
       <h1 className="text-3xl">Products</h1>
 
@@ -61,12 +60,26 @@ function App() {
       </form>
       <div className="app flex flex-wrap gap-4 mx-20 my-8">
         {products?.map((ele, index) => (
-          <div key={ele.id}  className="postcard ">
+          <div key={ele.id} className="postcard ">
             <PostCard data={ele}></PostCard>
           </div>
         ))}
       </div>
+      
     </div>
+    <div>
+        {newProducts.length > 0 && (
+          <div className="flex flex-col px-20">
+            <h1 className="text-3xl mb-4 ml-[45%]">New Products</h1>
+            <div className="flex gap-4 flex-wrap ">
+              {newProducts.map((ele,index) => (
+                <NewPostCard  key={index} data={ele}></NewPostCard>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+      </>
   );
 }
 
---
+++ src/components/NewPostCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NewPostCard = (props) => {
+  return (
+    <div className="w-[260px] h-[360px] bg-slate-100 rounded-md">
+      <img className='rounded-md'
+        src="https://images.pexels.com/photos/4158/apple-iphone-smartphone-desk.jpg"
+        alt=""
+      />
+      <div className='p-4'>
+        <p>{props.data.name}</p>
+        <p className="text-sm text-gray-600">{props.data.description}</p>
+        <p className="text-sm text-gray-600">Price : ${props.data.price}</p>
+        <p className="text-sm text-gray-600">Rating : 4</p>
+      </div>
+    </div>
+  );
+}
+
+export default NewPostCard
